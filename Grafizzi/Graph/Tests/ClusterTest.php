56dd19afe71a837fb4ce538a58dbedf989936ce4
Frederic G. MARAND <fgm@osinet.fr>
2012-05-21 18:59:51 +0200

    New tests for Cluster build, passing.

--- Grafizzi/Graph/Tests/ClusterTest.php
+++ Grafizzi/Graph/Tests/ClusterTest.php
@@ -5,6 +5,8 @@ namespace Grafizzi\Graph\Tests;
 require 'vendor/autoload.php';
 
 use Grafizzi\Graph\Cluster;
+use Grafizzi\Graph\Node;
+use Grafizzi\Graph\Edge;
 
 /**
  * Cluster test case.
@@ -31,6 +33,53 @@ class ClusterTest extends BaseGraphTest {
   protected function tearDown() {
     $this->Cluster = null;
     parent::tearDown();
+  }
+
+  public function testBuild() {
+    $n11 = new Node($this->dic, 'node11');
+    $n12 = new Node($this->dic, 'node12');
+    $e11_12 = new Edge($this->dic, $n11, $n12);
+    foreach (array($n11, $n12, $e11_12) as $element) {
+      $this->Cluster->addChild($element);
+    }
+    $expected = <<<EOT
+subgraph cluster_foo {
+  node11;
+  node12;
+  node11 -> node12;
+} /* /subgraph cluster_foo */
+
+EOT;
+
+    $build = $this->Cluster->build();
+    $this->assertEquals($expected, $build, 'Unbound cluster (2 nodes, 1 edge) built correctly.');
+
+    // Test bound subgraph build.
+    $this->Graph->addChild($this->Cluster);
+    $expected = <<<EOT
+  subgraph cluster_foo {
+    node11;
+    node12;
+    node11 -> node12;
+  } /* /subgraph cluster_foo */
+
+EOT;
+    $build = $this->Cluster->build();
+    $this->assertEquals($expected, $build, 'Bound cluster (2 nodes, 1 edge) built correctly.');
+
+    // Test graph with bound subgraph.
+    $expected = <<<EOT
+digraph G {
+  subgraph cluster_foo {
+    node11;
+    node12;
+    node11 -> node12;
+  } /* /subgraph cluster_foo */
+} /* /digraph G */
+
+EOT;
+    $build = $this->Graph->build();
+    $this->assertEquals($expected, $build, 'Graph with 1 cluster (2 nodes, 1 edge) built correctly.');
   }
 
   /**
